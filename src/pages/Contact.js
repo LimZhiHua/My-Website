390403cb44c231c6dcd49cbddec208c1e303b826
Lim Zhi Hua <thisiszhihua@gmail.com>
2021-11-28 03:26:55 -0500

    Adding singapore phone number

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,7 +8,7 @@ function Contact() {
         <div className='horizontalBlock'> 
               <div>
               <p style={{fontWeight:"bold", color:"white", fontSize:"30px", textAlign:"center"}}>Contact</p>
-              <p style={{fontWeight:"bold", color:"white", fontSize:"20px", textAlign:"center"}}>Phone Number: Will be updated once i get my Singapore phone number</p>
+              <p style={{fontWeight:"bold", color:"white", fontSize:"20px", textAlign:"center"}}>Phone Number: +65 97769822</p>
               <p style={{fontWeight:"bold", color:"white", fontSize:"20px", textAlign:"center"}}>Email: thisiszhihua@gmail.com</p>        
               </div>         
         </div>     
