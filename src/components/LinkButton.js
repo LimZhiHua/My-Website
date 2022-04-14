d51fbcd0abbcea7c3da4556dbc5c6a448387f66e
zh <thisiszhihua@gmail.com>
2022-04-14 15:35:29 +0800

    Gonna go try to make wordle on this branch

--- src/components/LinkButton.js
+++ src/components/LinkButton.js
@@ -11,7 +11,7 @@ export const LinkButton = ({children, type, onClick, buttonStyle, buttonSize}) =
     const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle: STYLES[0]
     const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize: SIZES[0]
     return(
-        <Link to="/sign-up" className="btn-mobile">
+        <Link to="/sign-up">
             <button className={`btn ${checkButtonStyle} ${checkButtonSize}`}
             onClick={onClick}
             type={type}>
--- src/pages/Testing.js
+++ src/pages/Testing.js
@@ -1,15 +1,18 @@
 import React, {useState, useEffect} from 'react';
 import "../App.css"
+import "./Testing.css"
 
 import { TextField, Grid} from '@material-ui/core' ;
 
+import CharacterTextField from '../components/CharacterTextField';
 import {baseURL} from "../urls";
 
 // Gonna try to make wordle
 // Is a 6x6 grid.
 function Testing() {
 
-    const [testGet, setGet] = React.useState(null);
+    const [testGet, setGet] = useState(null);
+    const [curRow, setCurRow] = useState(0);
     const usl  = baseURL +  "/testAPI"
     
     const getAPI = async () => {
@@ -39,7 +42,6 @@ function Testing() {
         console.log("Post res text is", postRes)
     };
 
-
     // <div>
     // <p style={{fontWeight:"bold", color:"white", fontSize:"30px", textAlign:"center"}}>This page is just for met to test random things</p>
     // <button onClick={ (e) => getAPI()}> getAPI</button>
@@ -47,42 +49,40 @@ function Testing() {
     // <button onClick={ (e) => postAPI()}>postAPI</button>
     // </div>         
 
+    
+    const rows = 5;
+    const cols = 6
 
-    const values = [0,1,2,3,4,5,6]
-    const gridItems =  values.map(value => {
+    // This just generates an array with equal size to the number of items
+    const values = Array.from(Array(rows * cols).keys())
+
+    const calculateDisabled = (index) => {
+        // we disable him if he is not on our current row
+        // currentRow's indexes can be calculated by going:
+        const startInd = (curRow * cols)
+        return !( startInd <= index )&& ( index <= (startInd + cols))
+    }
+
+    const nextRow = () => {
+        setCurRow(curRow + 1)
+        console.log("currow is", curRow)
+    }
+    
+    const gridItems = values.map(value => {
         return (
-            <Grid item key={value} xs={4}>
-                <p>{value}</p>
+            <Grid item xs={2}  className='gridItem'>
+                <CharacterTextField disabled={calculateDisabled(value)} value={value}/>
             </Grid>
         )
     })
     return (
-        <div className='horizontalBlock'> 
-            {/* <Grid contaner spacing = {2}>
-                <Grid item key={0} xs={4}>
-                    <p>0</p>
-                </Grid>
-                <Grid item key={1} xs={4}>
-                    <p>1</p>
-                </Grid>
-                <Grid item key={2} xs={4}>
-                    <p>2</p>
-                </Grid>
-            </Grid> */}
-        <Grid container spacing={2}>
-            <Grid item xs={6} md={8}>
-                <p>xs=6 md=8</p>
+        <div className='horizontalBlock '> 
+        <div className='centerDisplay'>
+            <Grid container spacing={1} className='gridContainer'>
+                {gridItems}
             </Grid>
-            <Grid item xs={6} md={4}>
-                <p>xs=6 md=4</p>
-            </Grid>
-            <Grid item xs={6} md={4}>
-                <p>xs=6 md=4</p>
-            </Grid>
-            <Grid item xs={6} md={8}>
-                <p>xs=6 md=8</p>
-            </Grid>
-        </Grid>
+        </div>
+        <button onClick={nextRow}>this is a button</button>
         </div>     
     );
   }
